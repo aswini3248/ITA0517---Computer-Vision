d7438dc78d85e246e2ab2259468a3095009aaed2
aswini3248 <113409861+aswini3248@users.noreply.github.com>
2024-03-20 09:04:36 +0530

    Update 3)Convert an Image to show outline using Canny function.py

--- 3)Convert an Image to show outline using Canny function.py
+++ 3)Convert an Image to show outline using Canny function.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 kernel = np.ones((5,5),np.uint8)
 print(kernel)
-path = "C:/Users/divya/OneDrive/Documents/COMPUTER VISION/CANNY.jpg"
+path = "C:/Users/aswin/OneDrive/Documents/COMPUTER VISION/CANNY.jpg"
 img =cv2.imread(path)
 imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 imgBlur = cv2.GaussianBlur(imgGray,(7,7),0)
