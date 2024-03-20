3b2528c110d9783d3f23c9b4e8ea4fcd1b0e7e56
aswini3248 <113409861+aswini3248@users.noreply.github.com>
2024-03-20 09:33:03 +0530

    Update 17)Edge detection using Sobel Matrix along X axis.py

--- 17)Edge detection using Sobel Matrix along X axis.py
+++ 17)Edge detection using Sobel Matrix along X axis.py
@@ -5,6 +5,5 @@ cv2.waitKey(0)
 img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 img_blur = cv2.GaussianBlur(img_gray, (3,3), 0)
 sobelx = cv2.Sobel(src=img_blur, ddepth=cv2.CV_64F, dx=1, dy=0, ksize=5) 
-the X axis
 cv2.imshow('Sobel X', sobelx)
 cv2.waitKey(0)
